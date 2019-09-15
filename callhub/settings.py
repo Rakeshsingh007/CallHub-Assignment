fd4a9576198afcde47d82e00d819e72335027742
rakesh <rakesh.s.shankala@gmail.com>
2019-09-15 22:25:07 +0530

    environment var

--- callhub/settings.py
+++ callhub/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f#funsrkil)hbs0jzot!x=)aefkq*r-4kfime)hc)vauw#k#hd'
+#SECRET_KEY = 'f#funsrkil)hbs0jzot!x=)aefkq*r-4kfime)hc)vauw#k#hd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['callhubappassignment.herokuapp.com']
 
 
 # Application definition
